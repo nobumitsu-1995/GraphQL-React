482cb757a74d47af03255d511c02de6cdd3de54c
nobumitsu_masugata <ys.training220208@gmail.com>
2025-06-22 09:58:55 +0900

    feat: prismaのマイグレーションを実行

---
+++ server/prisma/migrations/20250622004512_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateEnum
+CREATE TYPE "TodoStatus" AS ENUM ('PENDING', 'IN_PROGRESS', 'DONE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "status" "TodoStatus" NOT NULL DEFAULT 'PENDING',
+    "userId" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Todo_id_key" ON "Todo"("id");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
